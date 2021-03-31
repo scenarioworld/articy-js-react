8c6c3054ab8c0a0cdab954f7974ca45f97894999
Brook Warner Jensen <brwarner2@gmail.com>
2021-03-31 14:18:57 -0700

    Variables can be modified from the debug variable view.

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,43 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Variable, VariableStore } from 'articy-node';
+import { VariableDebugView } from "../src/components/VariableDebugView";
+import { useState } from 'react';
+import { useCallback } from 'react';
+
+const initial: VariableStore = {
+  "NamespaceOne": {
+    "MyVariable": 4,
+    "MyString": "HIII",
+    "MyBool": false,
+  },
+  "NamespaceTwo": {
+    "AnotherVar": 42,
+  }
+};
 
 const App = () => {
+  const [variables, setVariables] = useState(initial);
+
+  const setter = useCallback((ns: string, name: string, value: Variable) => {
+    const newVariables = {...variables};
+    newVariables[ns] = {...variables[ns]};
+    newVariables[ns][name] = value;
+    setVariables(newVariables);
+  }, [variables]);
+
+  const [show, setShow] = useState(false);
+
   return (
     <div>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <div>
+        <VariableDebugView store={variables} setVariable={setter}/>
+        <VariableDebugView store={variables} />
+      </div>}
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
--- src/components/VariableDebugView.tsx
+++ src/components/VariableDebugView.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Variable, VariableNamespace, VariableStore } from 'articy-node';
 import styles from './VariableDebugView.module.scss';
+import { useDebounce } from 'react-use';
+
+type SetVariableFn = (
+  namespace: string,
+  variable: string,
+  value: Variable
+) => void;
 
 interface Properties {
+  /** Variable store to display */
   store: VariableStore;
+
+  /** If set, this table can change variable values */
+  setVariable?: SetVariableFn;
 }
 
 function writeVariable(value: Variable) {
@@ -16,24 +27,128 @@ function writeVariable(value: Variable) {
   return value.toString();
 }
 
-function createVariableView(id: string, value: Variable) {
+function EditableVariable({
+  id,
+  value,
+  setVariable,
+}: {
+  id: string;
+  value: Variable;
+  setVariable: (value: Variable) => void;
+}): JSX.Element {
+  // Keep current value in ref
+  const valueRef = useRef(value);
+  valueRef.current = value;
+
+  // Keep track of internal state
+  const [state, setState] = useState(value);
+
+  // Make sure we change state when new values come in
+  useEffect(() => {
+    setState(value);
+  }, [value]);
+
+  // Change callback sets state
+  const type = typeof value;
+  const onChange = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
+      if (evt.target.type === 'checkbox') {
+        // Direct stream
+        setState(evt.target.checked);
+      } else if (type === 'number') {
+        setState(parseInt(evt.target.value));
+      } else {
+        setState(evt.target.value);
+      }
+    },
+    [type]
+  );
+
+  // Use debounce to set real value from state
+  useDebounce(
+    () => {
+      if (state !== value) {
+        // Set variable
+        setVariable(state);
+      }
+    },
+    500,
+    [state, value, setVariable]
+  );
+
+  // Create appropraite control
+  if (typeof state === 'string') {
+    return <input type="text" value={state} onChange={onChange} />;
+  } else if (typeof state === 'boolean') {
+    return (
+      <>
+        <input type="checkbox" checked={state} onChange={onChange} id={id} />
+        <label htmlFor={id}>({state.toString()})</label>
+      </>
+    );
+  } else {
+    return <input type="number" value={state} onChange={onChange} />;
+  }
+}
+
+function VariableView({
+  parent,
+  id,
+  value,
+  setVariable,
+}: {
+  parent: string;
+  id: string;
+  value: Variable;
+  setVariable?: (variable: string, value: Variable) => void;
+}) {
+  const setter = useCallback((v: Variable) => setVariable?.(id, v), [
+    setVariable,
+    id,
+  ]);
+
   return (
     <tr>
       <td>{id}</td>
-      <td>{writeVariable(value)}</td>
+      {setVariable !== undefined && (
+        <EditableVariable
+          id={parent + '.' + id}
+          value={value}
+          setVariable={setter}
+        />
+      )}
+      {setVariable === undefined && <td>{writeVariable(value)}</td>}
     </tr>
   );
 }
 
-function createNamespaceView(id: string, namespace: VariableNamespace) {
+function NamespaceView({
+  id,
+  namespace,
+  setVariable,
+}: {
+  id: string;
+  namespace: VariableNamespace;
+  setVariable?: SetVariableFn;
+}) {
+  const setter = useCallback(
+    (variable: string, value: Variable) => setVariable?.(id, variable, value),
+    [setVariable, id]
+  );
+
   return (
     <>
       <tr>
         <th>{id}</th>
       </tr>
-      {Object.keys(namespace).map(key =>
-        createVariableView(key, namespace[key])
-      )}
+      {Object.keys(namespace).map(key => (
+        <VariableView
+          parent={id}
+          id={key}
+          value={namespace[key]}
+          setVariable={setVariable ? setter : undefined}
+        />
+      ))}
     </>
   );
 }
@@ -43,9 +158,13 @@ export function VariableDebugView(props: Properties) {
   return (
     <table className={styles.variables}>
       <tbody>
-        {Object.keys(props.store).map(key =>
-          createNamespaceView(key, props.store[key])
-        )}
+        {Object.keys(props.store).map(key => (
+          <NamespaceView
+            id={key}
+            namespace={props.store[key]}
+            setVariable={props.setVariable}
+          />
+        ))}
       </tbody>
     </table>
   );
